6224674c14d79f2ffbcbe7b0dde990e375cca859
Camille Sylla <camillesylla.web@gmail.com>
2020-11-12 02:46:44 +0100

    added some more style to it

--- src/components/Shop/Views/Views.js
+++ src/components/Shop/Views/Views.js
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from 'react';
 
 
 import './Views.css'
-import Pic from '../../../assets/img/female/leggin.jpg';
 //items
 import { ItemContext } from '../../../context/ItemContext'
 
@@ -16,7 +15,7 @@ export default function Views() {
     } else {
         console.log(data);
         stuff = data.map((details, i) => {
-            const { name, brand, gender, price, image, categorie } = details;
+            const { name, brand, gender, price, image, category } = details;
 
             return (
             <div className="shopItemContainer">
@@ -25,7 +24,7 @@ export default function Views() {
                     </div>
                     <div className="shopDetails">
                         <p className="detailsName">{name}</p>
-                        <p className="brand">{brand}</p>
+                        <p className="brand">{category}</p>
                         <p className="gender">{gender}</p>
                         <p className="price">{price} €</p>
                     </div>
